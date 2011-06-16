48be85eb6e9d34ea4846731d7a99a7ddd8c858a2
Christopher M. Jones <cjones@partialflow.com>
2011-06-16 17:11:55 -0400

    remove site-name from page.tpl.php, add in styles to support using a mini panel in the header instead

--- themes/accuray/template.php
+++ themes/accuray/template.php
@@ -116,7 +116,7 @@ function accuray_preprocess_page(&$vars, $hook) {
 /**
  * Override or insert variables into the node templates.
  *
- * @param $vars
+ * @param $var 
  *   An array of variables to pass to the theme template.
  * @param $hook
  *   The name of the template being rendered ("node" in this case.)
