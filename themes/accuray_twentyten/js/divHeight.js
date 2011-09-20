85efdf272bb8e922beba15c402c89783c6a4e578
Christopher M. Jones <cjones@partialflow.com>
2011-09-20 08:52:58 -0400

    rebranding

---
+++ themes/accuray_twentyten/js/divHeight.js
@@ -0,0 +1,25 @@
+Drupal.behaviors.divHeight = function (context) {
+  var lengths = [];
+
+  lengths.push($(".region-sidebar-first").height());
+  lengths.push($("#content").height());
+  lengths.push($(".region-sidebar-second").height());
+  
+  var max = lengths[0];
+  
+  $(lengths).each(function(i, val){
+    if (val > max) {
+      max = val;
+    }
+  });
+  
+  var offset = $("#content").height() - $("#content .section").height();
+  $("#content .section").css({"min-height": max - offset});
+  
+  var offset = $(".region-sidebar-first").height() - $(".region-sidebar-first .section").height();
+  $(".region-sidebar-first .section").css({"min-height": max - offset});
+  
+  var offset = $(".region-sidebar-second").height() - $(".region-sidebar-second .section").height();
+  $(".region-sidebar-second .section").css({"min-height": max  - offset});
+
+}
---
+++ themes/accuray_twentyten/template.php
@@ -0,0 +1,230 @@
+<?php
+/**
+ * @file
+ * Contains theme override functions and preprocess functions for the theme.
+ *
+ * ABOUT THE TEMPLATE.PHP FILE
+ *
+ *   The template.php file is one of the most useful files when creating or
+ *   modifying Drupal themes. You can add new regions for block content, modify
+ *   or override Drupal's theme functions, intercept or make additional
+ *   variables available to your theme, and create custom PHP logic. For more
+ *   information, please visit the Theme Developer's Guide on Drupal.org:
+ *   http://drupal.org/theme-guide
+ *
+ * OVERRIDING THEME FUNCTIONS
+ *
+ *   The Drupal theme system uses special theme functions to generate HTML
+ *   output automatically. Often we wish to customize this HTML output. To do
+ *   this, we have to override the theme function. You have to first find the
+ *   theme function that generates the output, and then "catch" it and modify it
+ *   here. The easiest way to do it is to copy the original function in its
+ *   entirety and paste it here, changing the prefix from theme_ to accuray_twentyten_.
+ *   For example:
+ *
+ *     original: theme_breadcrumb()
+ *     theme override: accuray_breadcrumb()
+ *
+ *   where accuray is the name of your sub-theme. For example, the
+ *   zen_classic theme would define a zen_classic_breadcrumb() function.
+ *
+ *   If you would like to override any of the theme functions used in Zen core,
+ *   you should first look at how Zen core implements those functions:
+ *     theme_breadcrumbs()      in zen/template.php
+ *     theme_menu_item_link()   in zen/template.php
+ *     theme_menu_local_tasks() in zen/template.php
+ *
+ *   For more information, please visit the Theme Developer's Guide on
+ *   Drupal.org: http://drupal.org/node/173880
+ *
+ * CREATE OR MODIFY VARIABLES FOR YOUR THEME
+ *
+ *   Each tpl.php template file has several variables which hold various pieces
+ *   of content. You can modify those variables (or add new ones) before they
+ *   are used in the template files by using preprocess functions.
+ *
+ *   This makes THEME_preprocess_HOOK() functions the most powerful functions
+ *   available to themers.
+ *
+ *   It works by having one preprocess function for each template file or its
+ *   derivatives (called template suggestions). For example:
+ *     THEME_preprocess_page    alters the variables for page.tpl.php
+ *     THEME_preprocess_node    alters the variables for node.tpl.php or
+ *                              for node-forum.tpl.php
+ *     THEME_preprocess_comment alters the variables for comment.tpl.php
+ *     THEME_preprocess_block   alters the variables for block.tpl.php
+ *
+ *   For more information on preprocess functions and template suggestions,
+ *   please visit the Theme Developer's Guide on Drupal.org:
+ *   http://drupal.org/node/223440
+ *   and http://drupal.org/node/190815#template-suggestions
+ */
+
+
+/**
+ * Implementation of HOOK_theme().
+ */
+function accuray_theme(&$existing, $type, $theme, $path) {
+  $hooks = zen_theme($existing, $type, $theme, $path);
+  // Add your theme hooks like this:
+  /*
+  $hooks['hook_name_here'] = array( // Details go here );
+  */
+  // @TODO: Needs detailed comments. Patches welcome!
+  return $hooks;
+}
+
+/**
+ * Override or insert variables into all templates.
+ *
+ * @param $vars
+ *   An array of variables to pass to the theme template.
+ * @param $hook
+ *   The name of the template being rendered (name of the .tpl.php file.)
+ */
+/* -- Delete this line if you want to use this function
+function accuray_preprocess(&$vars, $hook) {
+  $vars['sample_variable'] = t('Lorem ipsum.');
+}
+// */
+
+/**
+ * Override or insert variables into the page templates.
+ *
+ * @param $vars
+ *   An array of variables to pass to the theme template.
+ * @param $hook
+ *   The name of the template being rendered ("page" in this case.)
+ */
+
+function accuray_preprocess_page(&$vars, $hook) {
+  
+  /**
+   * Conditionally set title and section_title. I know that context.module
+   * would be the better way to do this, but support for doing things with 
+   * vocabularies appears extraordinarily lacking in all the relevant modules
+   * (panels, context, views, ...). 
+   */
+
+  if (drupal_match_path($_GET['q'], 'taxonomy/term/*')) {
+    $term = taxonomy_get_term(arg(2));
+    $vocab = taxonomy_vocabulary_load($term->vid);
+    
+    $vars['section_title'] = $vocab->name;
+  }
+  
+  else if (!$vars['section_title']) {
+    $trail = menu_get_active_trail();
+  
+    if (count($trail) && 1 < count($trail)) {
+      $vars['section_title'] = $trail[1]['title'];
+    }
+  }
+  
+  // Prevent titles from becoming too long
+  $vars['title_short'] = truncate_utf8($vars['title'], $len=80, $wordsave=TRUE, $dots=TRUE);
+  $vars['section_title'] = truncate_utf8($vars['section_title'], $len=80, $wordsave=TRUE, $dots=TRUE);
+      
+  // To remove a class from $classes_array, use array_diff().
+  //$vars['classes_array'] = array_diff($vars['classes_array'], array('class-to-remove'));
+  //$vars['title'] = drupal_get_title();
+  if ($vars['title'] || $vars['section_title']) {
+    $vars['classes_array'][] = 'with-title';
+  }
+  
+  if ((!empty($vars['title']) || !empty($vars['section_title'])) && (!empty($vars['navigation']) || !empty($vars['primary_links']))) {
+    $vars['classes_array'][] = 'with-navigation-and-title';
+  }
+}
+// */
+
+/**
+ * Override or insert variables into the node templates.
+ *
+ * @param $var 
+ *   An array of variables to pass to the theme template.
+ * @param $hook
+ *   The name of the template being rendered ("node" in this case.)
+ */
+/* -- Delete this line if you want to use this function
+function accuray_preprocess_node(&$vars, $hook) {
+  $vars['sample_variable'] = t('Lorem ipsum.');
+
+  // Optionally, run node-type-specific preprocess functions, like
+  // accuray_preprocess_node_page() or accuray_preprocess_node_story().
+  $function = __FUNCTION__ . '_' . $vars['node']->type;
+  if (function_exists($function)) {
+    $function($vars, $hook);
+  }
+}
+// */
+
+/**
+ * Override or insert variables into the comment templates.
+ *
+ * @param $vars
+ *   An array of variables to pass to the theme template.
+ * @param $hook
+ *   The name of the template being rendered ("comment" in this case.)
+ */
+/* -- Delete this line if you want to use this function
+function accuray_preprocess_comment(&$vars, $hook) {
+  $vars['sample_variable'] = t('Lorem ipsum.');
+}
+// */
+
+/**
+ * Override or insert variables into the block templates.
+ *
+ * @param $vars
+ *   An array of variables to pass to the theme template.
+ * @param $hook
+ *   The name of the template being rendered ("block" in this case.)
+ */
+/* -- Delete this line if you want to use this function
+function accuray_preprocess_block(&$vars, $hook) {
+  $vars['sample_variable'] = t('Lorem ipsum.');
+}
+// */
+
+/**
+ * Return a themed breadcrumb trail.
+ *
+ * @param $breadcrumb
+ *   An array containing the breadcrumb links.
+ * @return
+ *   A string containing the breadcrumb output.
+ */
+function accuray_breadcrumb($breadcrumb) {
+  if (!count($breadcrumb)) {
+    $breadcrumb[] = l('Home', '<front>');
+  }  
+  
+  // Determine if we are to display the breadcrumb.
+  $show_breadcrumb = theme_get_setting('zen_breadcrumb');
+  if ($show_breadcrumb == 'yes' || $show_breadcrumb == 'admin' && arg(0) == 'admin') {
+
+    // Optionally get rid of the homepage link.
+    $show_breadcrumb_home = theme_get_setting('zen_breadcrumb_home');
+    if (!$show_breadcrumb_home) {
+      array_shift($breadcrumb);
+    }
+
+    // Return the breadcrumb with separators.
+    if (!empty($breadcrumb)) {
+      $breadcrumb_separator = theme_get_setting('zen_breadcrumb_separator');
+      $trailing_separator = $title = '';
+      if (theme_get_setting('zen_breadcrumb_title')) {
+        if ($title = drupal_get_title()) {
+          $trailing_separator = $breadcrumb_separator;
+        }
+      }
+      elseif (theme_get_setting('zen_breadcrumb_trailing')) {
+        $trailing_separator = $breadcrumb_separator;
+      }
+      return '<div class="breadcrumb">' . implode($breadcrumb_separator, $breadcrumb) . "$trailing_separator$title</div>";
+    }
+  }
+  // Otherwise, return an empty string.
+  return '';
+}
